a3df328c39b2f83c4e88c54f873d26fc8d80ad04
enzochristo <enzosc1@al.insper.edu.br>
2025-09-24 18:28:27 -0300

    4

--- exe4/main.c
+++ exe4/main.c
@@ -15,6 +15,9 @@
  const int PIN_ADC  = 28;
  repeating_timer_t timer0;
  repeating_timer_t timer1;
+ repeating_timer_t timerle;
+
+
 
 
  
@@ -23,10 +26,16 @@
 
  volatile int g_timer_0 = 0;
  volatile int g_timer_1 = 0;
+ volatile int le =0;
 
 
  bool timer_0_callback(repeating_timer_t *rt) {
         g_timer_0 = 1;
+        return true; // keep repeating
+    }
+
+ bool timer_le_callback(repeating_timer_t *rt) {
+        le = 1;
         return true; // keep repeating
     }
 
@@ -43,8 +52,11 @@
 
  int main() {
     stdio_init_all();
+
     adc_init();
     adc_gpio_init(28);
+    adc_select_input(2);
+
 
 
     uint16_t result;
@@ -52,71 +64,73 @@
     int ja_comecou_timer0 = 0;
     int ja_comecou_timer1 = 0;
 
-
     gpio_init(PIN_LED_B);
     gpio_put(PIN_LED_B,0);
     gpio_set_dir(PIN_LED_B, GPIO_OUT);
-    
+    add_repeating_timer_ms(10, timer_le_callback, NULL, &timerle);
 
-
-    gpio_init(PIN_ADC);
-    gpio_set_dir(PIN_ADC, GPIO_IN);
-    // o up e down sao referentes aos niveis de quando a tensao esta HIGH
-
+    // gpio_init(PIN_ADC);
+    // gpio_set_dir(PIN_ADC, GPIO_IN);
+    // // o up e down sao referentes aos niveis de quando a tensao esta HIGH
+    int led_status = 0;
 
     while (1) {
-   
-        adc_select_input(2);
-        result = adc_read();
-        voltagem = result * conversion_factor;
 
-        // verificar a voltagem e iniciar o timer.
-        printf("voltagem : %f\n", voltagem);
-
-        if (voltagem > 0){
-
-            if(voltagem < 2){
-                if(ja_comecou_timer0){
-                    add_repeating_timer_ms(300, timer_0_callback, NULL, &timer0);
-                    gpio_put(PIN_LED_B,1);
-                    ja_comecou_timer0 = 1;
-                }
-
-            }
-            else{
-                if (ja_comecou_timer1){
-                    add_repeating_timer_ms(500, timer_0_callback, NULL, &timer1);
-                    gpio_put(PIN_LED_B,1);
-                    ja_comecou_timer1 = 1;
-                }
-            }
-
+        
+        if (le){
+            result = adc_read();
+            voltagem = result * conversion_factor;
+            printf("%f\n", voltagem);
+            le = 0;
         }
-        else{
+        
+        if((voltagem >= 1.0) && (voltagem <= 2.0) && (ja_comecou_timer0 == 0)){
+            if(ja_comecou_timer1 == 1){
+                cancel_repeating_timer(&timer1);
+                ja_comecou_timer1 = 0;
+            }
+
+            printf("timer ativo \n");
+            add_repeating_timer_ms(300, timer_0_callback, NULL, &timer0);
+            // led_status = 1;
+            gpio_put(PIN_LED_B,1);
+            ja_comecou_timer0 = 1;
+        }
+
+        if((voltagem > 2.0) && (ja_comecou_timer1 == 0)){
+            if(ja_comecou_timer0 == 1){
+                cancel_repeating_timer(&timer0);
+                ja_comecou_timer0 = 0;
+            }
+
+            printf("timer ativo \n");
+            add_repeating_timer_ms(500, timer_1_callback, NULL, &timer1);
+            // led_status = 1;
+            gpio_put(PIN_LED_B,1);
+            ja_comecou_timer1 = 1;
+        }
+        if (!voltagem){
+            if((ja_comecou_timer0 == 1)){
+                cancel_repeating_timer(&timer0);
+                ja_comecou_timer0 = 0; 
+            }
+            else if( ja_comecou_timer1 == 1){
+                cancel_repeating_timer(&timer1);
+                ja_comecou_timer1 = 0; 
+            }
+            //     // g_timer_0 = 0;
+            //     // g_timer_1 = 0;
+            // }
             gpio_put(PIN_LED_B, 0);
         }
 
-        // vai osclilar entre ligar e desligar dado o timer passar de tempo. 
-        if(g_timer_0 && ja_comecou_timer0){
-            gpio_put(PIN_LED_B,0);
-            cancel_repeating_timer(&timer0);
-            ja_comecou_timer0 = 0;
+        if(g_timer_0 || g_timer_1){
+            led_status = !led_status;
+            gpio_put(PIN_LED_B,led_status);
+            g_timer_0 = 0;
+            g_timer_1 = 0;
         }
-
-        if(g_timer_1 && ja_comecou_timer1){
-            gpio_put(PIN_LED_B,0);
-            cancel_repeating_timer(&timer1);
-            ja_comecou_timer1 = 0;
-        }
-
-
-
-
-       
-
     }
-
-
-
-    }
+    
+}
 
